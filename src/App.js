1f85a8e2ea6c91527f6196a14fcfa61fdc6838ca
Katie Wahlstrom <misskatiebug@gmail.com>
2020-08-17 20:16:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ProductGrid from "./components/ProductGrid";
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "./ProductsService";
+import Modal from "./components/Modal";
+import AddEditProductForm from "./components/AddEditProductForm";
 function App() {
+  const [products, setProducts] = React.useState(() => {
+    fetchProducts();
+    return [];
+  });
+
+  const [
+    isShowingAddEditProductModal,
+    setIsShowingAddEditProductModal,
+  ] = React.useState(false);
+  const [currentProduct, setCurrentProduct] = React.useState(null);
+
+  function fetchProducts() {
+    getProducts()
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        debugger;
+      });
+  }
+
+  function handleAddProductClick() {
+    setIsShowingAddEditProductModal(true);
+  }
+
+  function handleCloseModal() {
+    setIsShowingAddEditProductModal(false);
+  }
+
+  function handleCreateProduct(product) {
+    createProduct(product)
+      .then((response) => {
+        setIsShowingAddEditProductModal(false);
+        alert("SUCCESSFULLY CREATE NEW ITEM");
+        fetchProducts();
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  function handleEditProduct(product) {
+    setCurrentProduct(product);
+    setIsShowingAddEditProductModal(true);
+  }
+
+  function handleUpdateProduct(product) {
+    updateProduct(product._id, product)
+      .then((response) => {
+        setIsShowingAddEditProductModal(false);
+        alert("SUCCESSFULLY UPDATED PRODUCT");
+        fetchProducts();
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
+
+  function handleDeleteProduct(product) {
+    deleteProduct(product._id)
+      .then((respons) => {
+        setIsShowingAddEditProductModal(false);
+        alert("SUCCESSFULLY DELETED PRODUCT");
+        fetchProducts();
+      })
+      .catch((errors) => {
+        alert(errors);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleAddProductClick}> CREATE NEW PRODUCT</button>
+      {isShowingAddEditProductModal ? (
+        <Modal>
+          <AddEditProductForm
+            existingProduct={currentProduct}
+            handleCloseModal={handleCloseModal}
+            handleCreateProduct={handleCreateProduct}
+            handleUpdateProduct={handleUpdateProduct}
+            handleDeleteProduct={handleDeleteProduct}
+          />
+        </Modal>
+      ) : null}
+      <h1>React Grocery App</h1>
+      <ProductGrid
+        products={products}
+        handleEditProduct={handleEditProduct}
+        handleUpdateProduct={handleEditProduct}
+        handleCreateProduct={handleCreateProduct}
+      />
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Modal.css";
+function Modal({ children }) {
+  return (
+    <div className="modal-container">
+      <div className="modal-content"> {children}</div>
+    </div>
+  );
+}
+
+export default Modal;
